82b0edcc281dd641222317b5de4d55b591154432
Waqas Sultan <waqas.sultan@xische.com>
2021-01-19 16:41:42 +0400

    Initial Commit

---
+++ PayPaliOSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayPaliOSTest
+//
+//  Created by Waqas Sultan on 19/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
